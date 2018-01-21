7767a9bd5e144b93b27fedd15309ec7aaac1b8f3
Martino Bettucci <martino.bettucci@maplink.global>
2018-01-21 20:02:48 +0100

    Adding the Cross Origin Resource Sharing layer to the API

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module ProgServeurRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '/v*/to_do*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
