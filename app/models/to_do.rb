f7cf3ab9e6c7fa37edec9adf8050814e8faec77f
Martino Bettucci <martino.bettucci@maplink.global>
2018-01-21 20:11:38 +0100

    Adding the VALIDE parameter

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -5,6 +5,7 @@ class ToDo < ApplicationRecord
         id: id,
         owner: owner,
         content: content,
+        valide: valide,
         date_creation: created_at.rfc2822,
         date_modification: updated_at.rfc2822
     }.as_json(options)
